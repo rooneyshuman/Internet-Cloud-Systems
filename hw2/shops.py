0bc776e2128bc3b7917d6685c9ce2ca5c6c9abb0
rooneyshuman <rooneyshuman@gmail.com>
2019-10-30 15:32:26 -0700

    Updated shops view and get method to retrieve and display the new table fields

--- hw2/shops.py
+++ hw2/shops.py
@@ -5,5 +5,6 @@ import gbmodel
 class Shops(MethodView):
     def get(self):
         model = gbmodel.get_model()
-        entries = [dict(name=row[0], email=row[1], signed_on=row[2], message=row[3] ) for row in model.select()]
-        return render_template('shops.html',entries=entries)
+        shops = [dict(name=row[0], street=row[1], city=row[2], state=row[3], zip=row[4], open_hr=row[5],
+                        close_hr=row[6], phone=row[7], drink=row[8], rating=row[9]) for row in model.select()]
+        return render_template('shops.html', shops=shops)
