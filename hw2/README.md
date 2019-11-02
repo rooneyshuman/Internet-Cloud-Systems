1e77db47d3594b320f909dbb6598645b952084ba
rooneyshuman <rooneyshuman@gmail.com>
2019-11-02 02:11:50 -0700

    Added project README

---
+++ hw2/README.md
@@ -0,0 +1,29 @@
+# Boba Guide
+
+Helping you get your bubble tea fix. Maintains a database of bubble tea shop locations. Allows users to view all locations or add a new one to the database.
+
+## Deployment
+
+The web-app has not yet been deployed, but can be launched locally through `python app.py` and reachable at `http://localhost:8000`.
+
+## Built With
+
+The following frameworks were used to build this full-stack web-app:
+
+* [Bootstrap4](https://getbootstrap.com/) - Styling framework
+* [Flask](https://palletsprojects.com/p/flask/) - Web application framework
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) - Web content structure
+* [Jinja2](https://palletsprojects.com/p/jinja/) - Template engine
+* [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3/) - Additional styling
+* [SQLite](https://www.sqlite.org/index.html) - Persistent data storage
+
+## Authors
+
+* **Belén Bustamante** - [rooneyshuman](https://bitbucket.org/bbelen)
+
+## License
+
+This project is licensed under the MIT License
+
+## Acknowledgements
+* Photos used were found on [Unsplash](https://unsplash.com/)
--- hw2/gbmodel/model_sqlite3.py
+++ hw2/gbmodel/model_sqlite3.py
@@ -23,6 +23,9 @@ DB_FILE = 'shops.db'  # file for our Database
 
 
 class model(Model):
+    """
+    Initializing method. Runs a 'COUNT' query to verify if the table exists. If an exception is returned, it creates the table.
+    """
     def __init__(self):
         # Make sure our database exists
         connection = sqlite3.connect(DB_FILE)
@@ -30,7 +33,7 @@ class model(Model):
         try:
             cursor.execute("SELECT COUNT(rowid) FROM shoplist")
         except sqlite3.OperationalError:
-            cursor.execute("CREATE TABLE shoplist (name text, street text, city text, state text, zip integer, open_hr text, close_hr text, phone text, website text, drink text, rating integer)")
+            cursor.execute("CREATE TABLE shoplist (name text, street text, city text, state text, zip integer, open_hr text, close_hr text, phone text, drink text, rating integer, website text)")
         cursor.close()
 
     def select(self):
@@ -44,7 +47,7 @@ class model(Model):
         try:
             cursor.execute("SELECT * FROM shoplist")
         except sqlite3.OperationalError:
-            cursor.execute("CREATE TABLE shoplist (name text, street text, city text, state text, zip integer, open_hr text, close_hr text, phone text, website text, drink text, rating integer)")
+            cursor.execute("CREATE TABLE shoplist (name text, street text, city text, state text, zip integer, open_hr text, close_hr text, phone text, drink text, rating integer, website text)")
         return cursor.fetchall()
 
     def insert(self, name, street, city, state, zip, open_hr, close_hr, phone, website, drink, rating):
@@ -69,8 +72,8 @@ class model(Model):
                   'close_hr': close_hr, 'phone': phone, 'website':website, 'drink': drink, 'rating': rating}
         connection = sqlite3.connect(DB_FILE)
         cursor = connection.cursor()
-        cursor.execute("insert into shoplist (name, street, city, state, zip, open_hr, close_hr, phone, website, drink, rating) "
-                       "VALUES (:name, :street, :city, :state, :zip, :open_hr, :close_hr, :phone, :website, :drink, :rating)",
+        cursor.execute("insert into shoplist (name, street, city, state, zip, open_hr, close_hr, phone, drink, rating, website) "
+                       "VALUES (:name, :street, :city, :state, :zip, :open_hr, :close_hr, :phone, :drink, :rating, :website)",
                        params)
 
         connection.commit()
