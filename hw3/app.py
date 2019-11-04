8d3dd2571d93c92e20ccf736f6d271656551b8c5
rooneyshuman <rooneyshuman@gmail.com>
2019-11-04 08:55:42 -0800

    Added hw3 directory and source files

---
+++ hw3/app.py
@@ -0,0 +1,25 @@
+"""
+A Flask application for viewing and submitting bubble tea store locations
+"""
+import flask
+from flask.views import MethodView
+from index import Index
+from form import Form
+from shops import Shops
+
+app = flask.Flask(__name__)       # our Flask app
+
+app.add_url_rule('/',
+                 view_func=Index.as_view('index'),
+                 methods=["GET"])
+
+app.add_url_rule('/form/',
+                 view_func=Form.as_view('form'),
+                 methods=['GET', 'POST'])
+
+app.add_url_rule('/shops/',
+                 view_func=Shops.as_view('shops'),
+                 methods=['GET', 'POST'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ hw3/gbmodel/model_sqlite3.py
@@ -0,0 +1,99 @@
+"""
+Flask app to store and retrieve a list of bubble tea shops
+Data is stored in a SQLite database that looks something like the following:
+
++------+--------------+----------+-------+-------+-----------+------------+--------------+-------------+------+--------+
+| Nam  | Address      | City     | State | Zip   | Open Hour | Close Hour | Phone        | Website     |Drink | Rating |
++======+==============+==========+=======+=======+===========+============+==============+=============+======+========+
+| Shop | 124 Some Ave | Portland | OR    | 97006 | 8:00 AM   | 5:00 PM    | 530-123-4567 | milktea.com | Taro | 5      |
++------+--------------+----------+-------+-------+-----------+------------+--------------+-------------+------+--------+
+
+This can be created with the following SQL (see bottom of this file):
+
+    create table shoplist (name text, street text, city text, state text, zip int, open_hr text, close_hr text,
+    phone text, website, drink text, rating int);
+
+
+"""
+from datetime import date
+from .Model import Model
+import sqlite3
+
+DB_FILE = 'shops.db'  # file for our Database
+
+
+class model(Model):
+    def __init__(self):
+        """
+        Initializing method. Runs a 'COUNT' query to verify if the table exists. If an exception is returned, it creates the table.
+        """
+        # Make sure our database exists
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT COUNT(rowid) FROM shoplist")
+        except sqlite3.OperationalError:
+            cursor.execute("CREATE TABLE shoplist (name text, street text, city text, state text, zip integer, open_hr text, close_hr text, phone text, drink text, rating integer, website text)")
+        cursor.close()
+
+    def select(self):
+        """
+        Gets all rows from the database
+        Each row contains: name, street, city, state, zip, open_hr, close_hr, phone, website, drink, rating
+        :return: List of lists containing all rows of database
+        """
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM shoplist")
+        except sqlite3.OperationalError:
+            cursor.execute("CREATE TABLE shoplist (name text, street text, city text, state text, zip integer, open_hr text, close_hr text, phone text, drink text, rating integer, website text)")
+        return cursor.fetchall()
+
+    def insert(self, name, street, city, state, zip, open_hr, close_hr, phone, website, drink, rating):
+        """
+        Inserts entry into database
+        :param name: String
+        :param street: String
+        :param city: String
+        :param state: String
+        :param zip: int
+        :param open_hr: String
+        :param close_hr: String
+        :param phone: String
+        :param website: String
+        :param drink: String
+        :param rating: int
+        :return: none
+        :raises: Database errors on connection and insertion
+        """
+
+        params = {'name': name, 'street': street, 'city': city, 'state': state, 'zip': zip, 'open_hr': open_hr,
+                  'close_hr': close_hr, 'phone': phone, 'website':website, 'drink': drink, 'rating': rating}
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO shoplist (name, street, city, state, zip, open_hr, close_hr, phone, drink, rating, website) "
+                       "VALUES (:name, :street, :city, :state, :zip, :open_hr, :close_hr, :phone, :drink, :rating, :website)",
+                       params)
+
+        connection.commit()
+        cursor.close()
+        return True
+
+    def delete(self, name):
+        """
+        Deletes an entry from the database
+        :param name: String
+        :return: none
+        :raises: Database errors on connection and deletion
+        """
+        connection = sqlite3.connect(DB_FILE)
+        try:
+            cursor = connection.cursor()
+            cursor.execute("DELETE FROM shoplist where name=?", (name,))
+            connection.commit()
+        except sqlite3.Error as error:
+            print("Failed to delete record from sqlite table", error)
+        finally:
+            if connection:
+                connection.close()
