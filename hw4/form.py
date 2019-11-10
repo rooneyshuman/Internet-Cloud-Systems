0c96489bb5fe795eccf40ce9685805b4729678ab
rooneyshuman <rooneyshuman@gmail.com>
2019-11-10 12:32:33 -0800

    Created hw4 folder. Adapted datastore info for boba shop

---
+++ hw4/form.py
@@ -0,0 +1,22 @@
+from flask import redirect, request, url_for, render_template
+from flask.views import MethodView
+import gbmodel
+
+class Form(MethodView):
+    def get(self):
+        """
+        GET method for the form page
+        :return: renders the form.html page on return
+        """
+        return render_template('form.html')
+
+    def post(self):
+        """
+        Accepts POST requests, and processes the form;
+        Redirect to index when completed.
+        """
+        model = gbmodel.get_model()
+        model.insert(request.form['name'], request.form['street'], request.form['city'], request.form['state'],
+                     request.form['zip'], request.form['open_hr'], request.form['close_hr'], request.form['phone'],
+                     request.form['website'], request.form['drink'], request.form['rating'])
+        return redirect(url_for('shops'))
